9231b8796b4bd4880e1e3c5d4f32842c1d86adc0
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-21 16:01:47 +0100

    ;

--- imc.js
+++ imc.js
@@ -6,10 +6,10 @@ const resultado = document.getElementById("resultado");
 calcular.addEventListener("click", () => {
     const imc = peso.value / ((altura.value / 100) ** 2);
     if (imc < 18.5) {
-        resultado.textContent = `Bajo peso: ${imc.toFixed(2)}`
+        resultado.textContent = `Bajo peso: ${imc.toFixed(2)}`;
     } else if (imc >= 18.5 && imc <= 24.9) {
-        resultado.textContent = `Peso normal: ${imc.toFixed(2)}`
+        resultado.textContent = `Peso normal: ${imc.toFixed(2)}`;
     } else {
-        resultado.textContent = `Sobrepeso: ${imc.toFixed(2)}`
+        resultado.textContent = `Sobrepeso: ${imc.toFixed(2)}`;
     }
 });
